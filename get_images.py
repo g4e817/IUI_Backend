be9ba8e93cba7cafd1a6081c20185f76862701b1
Markus Petritz <markus@mountain.co.at>
2023-01-27 17:17:20 +0100

    updates

--- get_images.py
+++ get_images.py
@@ -1,14 +1,22 @@
 import json
+import os.path
 import requests
-import re
+from tqdm import tqdm
+
+counter = 0
+num_lines = sum(1 for line in open('data/cleaned.jsonl'))
 
 with open('data/cleaned.jsonl') as f:
-    for line in f:
+    for line in tqdm(f, total=num_lines):
         item = json.loads(line)
         image_url = item['image']
-        image_data = requests.get(image_url).content
+        filename = os.path.basename(image_url)
+        filepath = f"data/images/{filename}"
 
-        match = re.match(r'media/recipe/(\d+)/conv/', image_url)
-        recipe_id = match.group(0)
-        with open(f"data/images/{recipe_id}.jpg", 'wb') as i:
+        if os.path.isfile(filepath):
+            # Skip already downloaded images
+            continue
+
+        image_data = requests.get(image_url).content
+        with open(filepath, 'wb') as i:
             i.write(image_data)
