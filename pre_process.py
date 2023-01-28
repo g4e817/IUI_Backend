9f62eb0a0b695740a7538caa6f6148da081892e7
Markus Petritz <markus@mountain.co.at>
2023-01-28 20:12:48 +0100

    new preprocessing

--- pre_process.py
+++ pre_process.py
@@ -24,19 +24,39 @@ def clean_cat(new_cat):
     new_cat = re.sub(r'\d+', ' ', new_cat)
     new_cat = re.sub(r'[^A-Za-z0-9]+', ' ', new_cat)
     new_cat = new_cat.strip()
-
-    if 'cocktail' in new_cat:
-        new_cat = 'cocktail'
-
     return new_cat
 
 
-stoplist = []
-with open('cat_stoplist.txt') as f:
-    for line in f:
-        line = clean_cat(line)
-        if len(line) > 2:
-            stoplist.append(line)
+def get_mapping(cat):
+    if cat in mapping:
+        return mapping[cat]
+    return []
+
+
+def invert_mapping(map):
+    new_map = {}
+    for key, vals in map.items():
+        for val in vals:
+            key = clean_cat(key)
+            val = clean_cat(val)
+            if val not in new_map:
+                new_map[val] = []
+            new_map[val].append(key)
+    return new_map
+
+
+#
+# stoplist = []
+# with open('cat_stoplist.txt') as f:
+#     for line in f:
+#         line = clean_cat(line)
+#         if len(line) > 2:
+#             stoplist.append(line)
+
+with open('new_categories.json') as f:
+    mapping = json.load(f)
+
+mapping = invert_mapping(mapping)
 
 unique_cats = set()
 
@@ -46,17 +66,18 @@ with open('data/cleaned.jsonl', 'w') as f:
             item = json.loads(line)
             if 'image' not in item or 'categories' not in item:
                 continue
-            cleaned_cats = []
+            cleaned_cats = set()
             for cat in item['categories'].split(","):
                 new_cat = cat.replace("Rezepte", "")
                 new_cat = clean_cat(new_cat)
-                if new_cat not in stoplist and len(new_cat) > 2:
-                    unique_cats.add(new_cat)
-                    cleaned_cats.append(new_cat)
+                new_cats = get_mapping(new_cat)
+                for ncat in new_cats:
+                    cleaned_cats.add(ncat)
+                    unique_cats.add(ncat)
             if len(cleaned_cats) <= 0:
                 continue
-            item['categories'] = cleaned_cats
-            f.write(json.dumps(item) + "\n")
+            out = {'categories': list(cleaned_cats), 'image': item['image']}
+            f.write(json.dumps(out) + "\n")
 
 print("unique", len(unique_cats))
 
